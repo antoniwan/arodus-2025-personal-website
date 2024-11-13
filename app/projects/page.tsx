ff27fed0242eb157133dea0fa9fa22a5627ab4cf
Antonio Rodriguez <antoniwan@icloud.com>
2024-11-13 16:34:23 -0500

    Project Page setup!

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+import { headingFont } from "@/utility/fonts";
+import ProjectItem from "@/components/ProjectItem";
+import kids_ebook_desktop from "@/assets/projects/ebook_kids-desktop.avif";
+import kids_ebook_tablet from "@/assets/projects/ebook_kids-ipad.avif";
+import kids_ebook_mobile from "@/assets/projects/ebook_kids-iphone.avif";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="">
+      <h1 className={`text-4xl font-black ${headingFont.className} mb-12`}>
+        Recent Projects
+      </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+      <ProjectItem
+        title="AbogadaJulia.com"
+        type="Immigration Lawyer CMS Website"
+        url="https://www.abogadajulia.com"
+        desktopScreenshot={kids_ebook_desktop}
+        mobileScreenshot={kids_ebook_mobile}
+        tabletScreenshot={kids_ebook_tablet}
+      >
+        <p>
+          I used the{" "}
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://create-react-app.dev"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            create-react-app
+          </a>{" "}
+          React starter to create a simple e-book website. It uses{" "}
+          <a
+            href="https://styled-components.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Styled Components
           </a>
+          , React hooks, and context to manage state and local storage to save
+          the chosen language and the last page the user browsed. I made the
+          illustrations using multiple AI generators, and writing the content
+          was the most challenging part. The design is simple, although entirely
+          responsive.
+        </p>
+      </ProjectItem>
+
+      <ProjectItem
+        title="Mia, the Sun, and the Moon"
+        type="Children's E-book in English and Spanish"
+        url="http://mia-the-sun-and-the-moon.antoniorodriguez.us/"
+        desktopScreenshot={kids_ebook_desktop}
+        mobileScreenshot={kids_ebook_mobile}
+        tabletScreenshot={kids_ebook_tablet}
+      >
+        <p>
+          I used the{" "}
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://create-react-app.dev"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            create-react-app
+          </a>{" "}
+          React starter to create a simple e-book website. It uses{" "}
+          <a
+            href="https://styled-components.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Styled Components
           </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          , React hooks, and context to manage state and local storage to save
+          the chosen language and the last page the user browsed. I made the
+          illustrations using multiple AI generators, and writing the content
+          was the most challenging part. The design is simple, although entirely
+          responsive.
+        </p>
+      </ProjectItem>
     </div>
   );
 }
---
+++ components/ProjectItem.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import { headingFont } from "@/utility/fonts";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+
+const ProjectItem = ({
+  title,
+  type,
+  url,
+  children,
+  desktopScreenshot,
+  mobileScreenshot,
+  tabletScreenshot,
+}: {
+  title: string;
+  type: string;
+  url: string;
+  children: React.JSX.Element;
+  desktopScreenshot: StaticImport;
+  mobileScreenshot: StaticImport;
+  tabletScreenshot: StaticImport;
+}) => {
+  return (
+    <div className="flex flex-row flex-wrap bg-gray-100 dark:bg-stone-900 rounded-lg mb-8">
+      <div className="flex flex-row flex-wrap p-8 border-solid border-b border-gray-300 dark:border-gray-700">
+        <h2
+          className={`${headingFont.className} font-black text-2xl mb-0 basis-full`}
+        >
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h2>
+
+        <h3
+          className={`${headingFont.className} text-stone-700 dark:text-stone-200 font-bold text-xl basis-full`}
+        >
+          {type}
+        </h3>
+        {children}
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 m:h-80 p-8 overflow-hidden">
+        <Image
+          className="object-scale-down rounded-lg"
+          src={desktopScreenshot}
+          alt="Desktop Screenshot of a children's ebook"
+        />
+        <Image
+          className="object-scale-down rounded-lg"
+          src={mobileScreenshot}
+          alt="Mobile Screenshot of a children's ebook"
+        />
+        <Image
+          className="object-scale-down rounded-lg"
+          src={tabletScreenshot}
+          alt="Tablet Screenshot of a children's ebook"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
